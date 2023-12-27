7c90ebd534e88195ab2a51148be1f24d48923df9
douniabenabdelali <dounia.benabdelali@student.ehb.be>
2023-12-27 14:38:57 +0100

    spel vragen-toegevoegd

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const addTaskBtn = document.getElementById('addTaskBtn');
     const addTaskButton = document.getElementById('addTaskButton');
     const closeBtn = document.querySelector('.close');
-  
+    const questionModal = document.getElementById('questionModal');
+    const questionText = document.getElementById('questionText');
+    const choices = document.getElementById('choices');
+    const scoreDisplay = document.getElementById('scoreDisplay');
+    let score = 0;
+
+
     columns.forEach(column => {
       column.addEventListener('dragover', dragOver);
       column.addEventListener('dragenter', dragEnter);
@@ -37,6 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
       this.appendChild(dragging);
       this.classList.remove('dragover');
       setTaskColor(dragging, this.id);
+      askQuestion(); // Stel een vraag bij elke gesleepte taak
     }
   
     function addTask(e) {
@@ -93,6 +100,149 @@ document.addEventListener('DOMContentLoaded', () => {
         event.target.classList.remove('dragging');
       }
     });
+
+    function askQuestion() {
+      const questions = [
+        {
+          question: "Wat is het doel van een firewall in een computernetwerk?",
+          choices: [
+            "Data opslaan",
+            "Toegang tot een netwerk beheren",
+            "E-mails verzenden",
+            "Webpagina's maken"
+          ],
+          correctAnswer: 1
+        },
+        {
+          question: "Wat is de betekenis van 'HTML'?",
+          choices: [
+            "Hypertext Markup Language",
+            "Hyperlinking Text Management Language",
+            "Hyper Tool Multi Language",
+            "Home Tool Markup Language"
+          ],
+          correctAnswer: 0
+        },
+        {
+          question: "Wat is de rol van een 'Compiler' in programmeren?",
+          choices: [
+            "Het uitvoeren van code",
+            "Het vertalen van code naar machinecode",
+            "Het debuggen van code",
+            "Het opslaan van code"
+          ],
+          correctAnswer: 1
+        },
+        {
+          question: "Wat is het doel van een 'Cookie' in webontwikkeling?",
+          choices: [
+            "Om de website op te eten",
+            "Om data op te slaan op de server",
+            "Om data op te slaan op de client",
+            "Om data te verwijderen"
+          ],
+          correctAnswer: 2
+        },
+        {
+          question: "Wat is de betekenis van 'LAN' in computernetwerken?",
+          choices: [
+            "Local Area Network",
+            "Long Antenna Node",
+            "Large Access Network",
+            "Low-latency Adapter Node"
+          ],
+          correctAnswer: 0
+        },
+        {
+          question: "Wat doet een 'Cache' in een computersysteem?",
+          choices: [
+            "Data opslaan voor toekomstig gebruik",
+            "Data permanent verwijderen",
+            "Het monitoren van het systeem",
+            "Het veranderen van wachtwoorden"
+          ],
+          correctAnswer: 0
+        },
+        {
+          question: "Wat is een 'Boolean' in de informatica?",
+          choices: [
+            "Een type variabele dat enkel nullen bevat",
+            "Een algoritme voor rekenkunde",
+            "Een type variabele dat true of false kan bevatten",
+            "Een programmeertaal voor games"
+          ],
+          correctAnswer: 2
+        },
+        {
+          question: "Wat is de functie van een 'Router' in een netwerk?",
+          choices: [
+            "Verbinding maken met externe apparaten",
+            "Data opslaan op een lokale schijf",
+            "Het doorsturen van data tussen netwerken",
+            "Het omzetten van data naar geluid"
+          ],
+          correctAnswer: 2
+        },
+        {
+          question: "Wat is een 'Algoritme'?",
+          choices: [
+            "Een kookrecept",
+            "Een speciale code voor computerbeveiliging",
+            "Een stappenplan voor probleemoplossing",
+            "Een type programmeertaal"
+          ],
+          correctAnswer: 2
+        },
+        {
+          question: "Wat betekent 'GUI' in de informatica?",
+          choices: [
+            "Graphical User Interface",
+            "General Utility Interface",
+            "Global Unification Interface",
+            "Graphical Utility Interaction"
+          ],
+          correctAnswer: 0
+        }
+      ];
+      
+  
+      const randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+  
+      questionText.textContent = randomQuestion.question;
+      choices.innerHTML = "";
+  
+      randomQuestion.choices.forEach((choice, index) => {
+        const button = document.createElement('button');
+        button.textContent = choice;
+        button.classList.add('choiceBtn');
+        button.addEventListener('click', () => {
+          if (index === randomQuestion.correctAnswer) {
+            score += 10;
+            scoreDisplay.textContent = `Je score: ${score}`;
+            showNotification("Correct antwoord! +10 punten", "success");
+          } else {
+            showNotification("Fout antwoord", "error");
+          }
+          questionModal.style.display = 'none';
+        });
+        choices.appendChild(button);
+      });
+  
+      questionModal.style.display = 'block';
+    }
+
+    function showNotification(message, type) {
+      const notification = document.createElement("div");
+      notification.className = `notification ${type}`;
+      notification.textContent = message;
+  
+      document.body.appendChild(notification);
+  
+      setTimeout(() => {
+        document.body.removeChild(notification);
+      }, 3000);
+    }
+    
     function login() {
       var username = document.getElementById("username").value;
       var password = document.getElementById("password").value;
